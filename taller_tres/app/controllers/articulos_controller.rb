be5fdb7b7433590e3b202b85f37ef3dad0de96a9
davidlruizc <davidl-ruizc@outlook.com>
2020-09-16 22:00:07 -0500

    add models, views and controllers for articles

---
+++ taller_tres/app/controllers/articulos_controller.rb
@@ -0,0 +1,15 @@
+class ArticulosController < ApplicationController
+  def new
+  end
+
+  def create
+    @articulo = Articulo.new(params[:articulo])
+
+    @articulo.save
+    redirect_to :action => :show, :id => @articulo.id
+  end
+
+  def show
+    @articulo = Articulo.find(params[:id])
+  end
+end
